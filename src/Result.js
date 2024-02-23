a189a352576ffbad0750f1dec5b33d1cb1e20082
Malarnithi <malarnithivelusamy21@gmail.com>
2024-02-23 11:29:48 +0530

    update

--- src/Result.js
+++ src/Result.js
@@ -1,24 +1,20 @@
-import React from "react";
-
-export default function Result({secretNum, term}) {
+export default function Result({ secretNum, term }) {
     let result;
-    
-    if(term){
-    if(term>secretNum) {
-         result='Higher';
-    }
-    else if(term<secretNum) {
-        result='Lower';
-    }
-    else if(term==secretNum) {
-        result='Wow! You found it';
-    }
-    else{
-        result='Enter valid input';
-    }
-    } 
 
-    return(
+    if (term) {
+        const termNumber = parseInt(term); // Convert term to a number
+        if (termNumber > secretNum) {
+            result = 'Higher';
+        } else if (termNumber < secretNum) {
+            result = 'Lower';
+        } else if (termNumber === secretNum) {
+            result = 'Wow! You found it';
+        } else {
+            result = 'Enter valid input';
+        }
+    }
+
+    return (
         <h3 className="result">You Guessed: {result}</h3>
-    )
+    );
 }
